7900551666b1feb50712ae0a9306628c7051a159
jessejamesss <jessejames.ro@gmail.com>
2023-11-10 22:36:17 -0800

    Initial caption cleaning

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,7 @@
+import re
+import time
 import psycopg2
+import itertools
 import config as cfg
 
 def connectDB():
@@ -18,13 +21,39 @@ def connectDB():
     return conn, cur
 
 def getCaptions(cur):
-    SQL = f'SELECT captions FROM posts WHERE captions != "Caption does not exist."'
+    SQL = f'SELECT caption FROM posts WHERE caption <> \'Caption does not exist.\''
     try:
         cur.execute(SQL)
     except psycopg2.Error as e:
         print('ERROR: getCaptions query unsuccessful.')
         print(e)
 
+    captions = cur.fetchall()
+    captions = list(itertools.chain(*captions))
+    
+    return captions
+
+def transform(captions):
+    hasSymbol = []
+    for caption in captions:
+        if '🎶' in caption:
+            hasSymbol.append(caption)
+
+    return hasSymbol
 
 if __name__ == '__main__':
-    cur, conn = connectDB()
+    start = time.time()
+    conn, cur = connectDB()
+    captions = getCaptions(cur)
+
+    withMusic = transform(captions)
+    for caption in withMusic:
+        pattern = re.compile(r'\b(.+)\s-\s(.+)\b')
+        match = pattern.finditer(caption)
+
+        for m in match:
+            s = re.sub(r'\(.*?\)|\(.*?\w*|\[.*?\]+|@\w+/g',"",str(m.group(0)))
+            print(s)
+
+
+    print(time.time() - start)
