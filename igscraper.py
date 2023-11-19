6c0606447732353f2a940fa505627c52ed2f4a1f
jessejamesss <jessejames.ro@gmail.com>
2023-11-19 13:29:36 -0800

    Final: Implemented WebDriverWait and checks for existing records

--- igscraper.py
+++ igscraper.py
@@ -1,47 +1,16 @@
-import time
 import psycopg2
 import config as cfg
+from db import Database
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-
-def dbConnect():
-    # Establish a connection to database
-    try:
-        conn = psycopg2.connect(f'host=localhost dbname=sleepingongems user=postgres password={cfg.dbPassword}')
-    except psycopg2.Error as e:
-        print('ERROR: Connection to sleepingongems unsuccessful.')
-        print(e)
-
-    # Initialize cursor to execute queries
-    try:
-        cur = conn.cursor()
-    except psycopg2.Error as e:
-        print('ERROR: Cursor initialization unsuccessful.')
-        print(e)
-
-    return conn, cur
-
-def createTable(cur):
-    # Create table to store collected data
-    SQL = 'CREATE TABLE IF NOT EXISTS posts (id SERIAL PRIMARY KEY, \
-                                             url VARCHAR(50) NOT NULL, \
-                                             date DATE, \
-                                             caption TEXT NOT NULL, \
-                                             likes INTEGER, \
-                                             CONSTRAINT link UNIQUE(url));'
-    try:
-        cur.execute(SQL)
-    except psycopg2.Error as e:
-        print('ERROR: Posts table creation unsuccessful.')
-        print(e)
 
 def login(driver):
     # Enter Username and Password –> Click 'Login'
-    time.sleep(2)
-
-    username = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
-    password = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
+    username = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="username"]')))
+    password = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[name="password"]')))
 
     username.clear()
     password.clear()
@@ -49,30 +18,23 @@ def login(driver):
     username.send_keys(cfg.username)
     password.send_keys(cfg.password)
 
-    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[type="submit"]'))).click()
 
     # Handle Save Login Info –> Click 'Not Now'
-    time.sleep(5)
-    driver.find_element(By.XPATH, '//div[contains(text(), "Not Now")]').click()
-
-    time.sleep(5)
-    driver.find_element(By.XPATH, '//button[contains(text(), "Not Now")]').click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//div[contains(text(), "Not Now")]'))).click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
 
 def search(driver):
-    time.sleep(2)
     # Click Search Icon
-    driver.find_element(By.CSS_SELECTOR, 'svg[aria-label="Search"]').click()
-    time.sleep(2)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'svg[aria-label="Search"]'))).click()
 
     # Search sleepingongems in searchbox
-    searchbox = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Search"]')
+    searchbox = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Search"]')))
     searchbox.clear()
     searchbox.send_keys('sleepingongems')
-    time.sleep(2)
 
     # Click search result
-    driver.find_element(By.CSS_SELECTOR, 'a[href="/sleepingongems/"]').click()
-    time.sleep(2)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="/sleepingongems/"]'))).click()
 
 def balance(caption):
     if "'" in caption or "’" in caption:
@@ -81,76 +43,76 @@ def balance(caption):
         
     return caption
 
-def collect(driver, cur):
+def collect(driver):
     # Click initial post
-    time.sleep(2)
-    driver.find_element(By.CSS_SELECTOR, 'div[class="_aabd _aa8k  _al3l"]').click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[class="_aabd _aa8k  _al3l"]'))).click()
 
-    # Create SQL query
-    SQL = "INSERT INTO posts (url, date, caption, likes) VALUES ('{}', '{}', '{}', '{}');"
     while True:
-        time.sleep(2)
         # Collect data
-        div = driver.find_element(By.CSS_SELECTOR, 'div[class="x1cy8zhl x9f619 x78zum5 xl56j7k x2lwn1j xeuugli x47corl"]')
+        div = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class="x1cy8zhl x9f619 x78zum5 xl56j7k x2lwn1j xeuugli x47corl"]')))
         url = driver.current_url.rsplit('/', 1)[:-1][0] +'/'
         try:
             caption = balance(div.find_element(By.XPATH, './/h1').text)
         except NoSuchElementException:
             caption = 'Caption does not exist.'
             pass
-        date = div.find_element(By.CSS_SELECTOR, 'time[class="_aaqe"]').get_attribute('datetime').split('T')[0]
-        section = div.find_element(By.CSS_SELECTOR, 'section[class="_ae5m _ae5n _ae5o"]')
+        date = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'time[class="_aaqe"]'))).get_attribute('datetime').split('T')[0]
+        section = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'section[class="_ae5m _ae5n _ae5o"]')))
         likes = int(section.find_element(By.CSS_SELECTOR, '.html-span').text.replace(',',""))
         print('URL: ' + str(url) +
               '\nCaption: ' + caption +
               '\nDate: ' + date +
               '\nLikes: ' + str(likes))
         
-        # Check if record exists in database
-        try:
-            print('CHECK GOES HERE')
-            # break
-        except psycopg2.Error as e:
-            print(e)
+        with Database() as db:
+            db.set_session(commit=True)
 
-        # Insert data record into database
-        try:
-            cur.execute(SQL.format(url, date, caption, likes))
-            print('^ SUCCESS: Inserted ^')
-        except psycopg2.Error as e:
-            print('^ ERROR: Record insertion unsuccessful. ^')
-            print(e)
-            pass
+            # Create SQL queries
+            insertSQL = "INSERT INTO posts (url, date, caption, likes) VALUES ('{}', '{}', '{}', '{}');"
+            checkSQL = "SELECT url FROM posts WHERE url = '{}';"
+
+            # Check if record exists in database
+            # If record exists, break (all posts have been added to database)
+            try:
+                db.execute(checkSQL.format(url))
+                if db.fetchone() is not None:
+                    print('^ Post exists in database... Ending collection ^')
+                    break
+            except psycopg2.Error as e:
+                print('^ ERROR: Record check unsuccessful. ^')
+                print(e)
+
+            # Insert data record into database
+            try:
+                db.execute(insertSQL.format(url, date, caption, likes))
+                print('^ SUCCESS: Inserted ^')
+            except psycopg2.Error as e:
+                print('^ ERROR: Record insertion unsuccessful. ^')
+                print(e)
+                pass
 
         # Click Next arrow, if element does not exist break out of loop (last post reached)
         # Exception should only be reached on first scrape collection
         try:
-            driver.find_element(By.CSS_SELECTOR, 'svg[aria-label="Next"]').click()
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'svg[aria-label="Next"]'))).click()
         except NoSuchElementException:
             print("Reached last post.")
             break
 
-# Connect to SleepingOnGems database & Initialize cursor
-conn, cur = dbConnect()
-conn.set_session(autocommit=True)
+if __name__ == '__main__':
+    # Initialize Chrome webdriver and go to Instagram
+    driver = webdriver.Chrome()
+    driver.get("https://www.instagram.com")
 
-# Create table in database
-createTable(cur)
+    # Login to Instagram
+    login(driver)
 
-# Initialize Chrome webdriver and go to Instagram
-driver = webdriver.Chrome()
-driver.get("https://www.instagram.com")
+    # Search SleepingOnGems profile
+    search(driver)
 
-# Login to Instagram
-login(driver)
+    # Begin Scraping
+    collect(driver)
 
-# Search SleepingOnGems profile
-search(driver)
+    # Close driver, cursor, and connection
+    driver.quit()
 
-# Begin Scraping
-collect(driver, cur)
-
-# Close driver, cursor, and connection
-driver.quit()
-cur.close()
-conn.close()
